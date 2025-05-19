1fed3a43f8e72f1248c009099ef307f2773c77ae
Braxgjider <168082518+Braxgjider@users.noreply.github.com>
2025-05-19 22:54:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let validat = false;    // variable que permet saber si hi ha algun usuari validat
+let validat = false;
 let nom, contrasenya;
-let scriptURL = "https://script.google.com/.../exec"    // s'ha de substituir la cadena de text per la URL del script
+let scriptURL = "https://braxgjider.github.io/Unaicrespo1892/";
 
-function inici_sessio() {
-    nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
-    contrasenya = document.getElementById("contrasenya").value;
-    let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
-    fetch(consulta)
-        .then((resposta) => {   // registres que contenen el nom d'usuari i contrasenya escrits per l'usuari
-            return resposta.json();    // conversió a llista
-        })
-        .then((resposta) => {
-            if(resposta.length == 0) {    // llista buida
-                window.alert("El nom d'usuari o la contrasenya no són correctes.");
-            }
-            else {    // llista amb (almenys) un registre
-                window.alert("S'ha iniciat correctament la sessió.");
-                inicia_sessio();    // usuari validat, s'executen les instruccions del procediment "inicia_sessio"
-            }
-        });    
+function inicia_sessio() {
+  nom = document.getElementById("nom_usuari").value;
+  contrasenya = document.getElementById("contrasenya").value;
+  let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
+
+  fetch(consulta)
+    .then((resposta) => resposta.json())
+    .then((resposta) => {
+      if (resposta.length == 0) {
+        window.alert("El nom d'usuari o la contrasenya no són correctes.");
+      } else {
+        window.alert("S'ha iniciat correctament la sessió.");
+        validat = true;
+        canvia_seccio(1); // Canvia a una secció inicial adequada
+      }
+    });
 }
